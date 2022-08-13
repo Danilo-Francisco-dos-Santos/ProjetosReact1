ad318814fa0d38d1609119c3919dee60cc176a82
Danilo Santos <danilohhe@gmail.com>
2022-08-12 22:30:06 -0300

    Imagem anexada a página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,16 @@ import React from "react";
 
 import { ContainerPage, TitlePage } from "../../components/Main";
 
+// Como passar imagens para a página
+const url = 'https://via.placeholder.com/150'
+
 function pageHome() {
     return (
         <ContainerPage>
             <TitlePage>Página principal</TitlePage>
+            <div>
+                <img src={url} alt="Minha Imagem"></img>
+            </div>
         </ContainerPage>
     );
 }
